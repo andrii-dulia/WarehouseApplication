bd0e85775febd54c3f67c3a60134ae8a9abf5fbb
Andrii Dulia <av.duliya@gmail.com>
2020-09-05 09:17:45 +0100

    3 commit

--- src/main/java/util/OrderDetailsService.java
+++ src/main/java/util/OrderDetailsService.java
@@ -1,7 +1,6 @@
 package util;
 
 import model.OrderDetails;
-import model.Product;
 
 import java.util.Scanner;
 
@@ -9,20 +8,16 @@ public class OrderDetailsService {
 
     public OrderDetails createSinglePositionInOrder(){
 
-        //OrderDetails orderDetails=new OrderDetails();
+        OrderDetails orderDetails=new OrderDetails();
 
         Scanner scanner=new Scanner(System.in);
         System.out.println("Please provide product id");
 
         int providedId=scanner.nextInt();
-        ProductService productService=new ProductService();
-        Product product=productService.getProductById(providedId);
 
-        System.out.println("Please provide quantity");
 
-        int quantity=scanner.nextInt();
 
-        OrderDetails orderDetails1=new OrderDetails(product,quantity);
+
 
 
 
